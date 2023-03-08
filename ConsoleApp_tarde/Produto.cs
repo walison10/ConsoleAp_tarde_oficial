b53b1a4bcd9f7b1223eecba6cfa7431677381bb8
WALISON10 <walison.xavier@fatec.sp.gov.br>
2023-03-08 14:23:10 -0300

    crie os atributos e o metodo construtor

--- ConsoleApp_tarde/Produto.cs
+++ ConsoleApp_tarde/Produto.cs
@@ -8,5 +8,15 @@ namespace ConsoleApp_tarde
 {
     internal class Produto
     {
+        private int id;
+        private string Descricao;
+        private decimal Valor;
+
+        public Produto(int id, string descricao, decimal valor) 
+        {
+            id = id;
+            Descricao = descricao
+                Valor = valor;
+        }
     }
 }
