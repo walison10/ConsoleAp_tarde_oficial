8ffe0bde9b40679d39a7dacf7bfb444b53d7f1cb
WALISON10 <walison.xavier@fatec.sp.gov.br>
2023-03-08 16:23:09 -0300

    Foi criado classe cliente e endereço com construtor e os metodos get e set

---
+++ ConsoleApp_tarde/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_tarde
+{
+    internal class Cliente
+    {
+
+        private int Id;
+        private string Nome;
+        private string Tel;
+
+        public Cliente(int id, string Nome, string Tel)
+        {
+            Id = id;
+            Nome = "WALISON";
+            Tel = "16-99785 4586";
+        }
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public string getNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+        public string getTel()
+        {
+            return Tel;
+        }
+        public void setTel(string tel)
+        {
+            Tel = tel;  
+        }
+    }
+}
+    
+
---
+++ ConsoleApp_tarde/Endereco.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_tarde
+{
+    internal class Endereco
+    {
+
+        private int Id;
+        private string Cep;
+        private string Rua;
+        private string Numero;
+        private string Bairro;
+        private string Cidade;
+
+        public Endereco(int id, string cep, string rua, string numero, string bairro, string cidade)
+        {
+            Id = id;
+            Cep = cep;
+            Rua = rua;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+
+
+
+        }
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public string getCep()
+        {
+            return Cep;
+        }
+        public void setCep(string cep)
+        {
+            Cep = cep;
+        }
+        public string getRua()
+        {
+            return Rua;
+        }
+        public void setRua(string rua)
+        {
+            Rua = rua;
+        }
+        public string getNumero()
+        {
+            return Numero;
+        }
+        public void setNumero(string numero)
+        {
+            Numero = numero;
+        }
+        public string getBairro()
+        {
+            return Bairro;
+        }
+        public void setBairro(string bairro)
+        {
+            Bairro = bairro;
+        }
+        public string getCidade() 
+        {
+            return Cidade;
+        }
+        public void setCidade(string cidade)
+        {
+            Cidade = cidade;
+        }
+    }
+}
